cefbe5823d4ad6a5aecaaf7336b2be529c66fe30
Redmine development <redmine@florence.localdomain>
2010-05-17 17:34:32 +0100

    changed readme and added task_import model

---
+++ app/models/task_import.rb
@@ -0,0 +1,18 @@
+########################################################################
+# File:    loader.rb                                                   #
+#          Based on work by Hipposoft 2008                             #
+#                                                                      #
+# Purpose: Encapsulate data required for a loader session.             #
+#                                                                      #
+# History: 16-May-2008 (ADH): Created.                                 #
+#          Feb 2009 (SJS): Hacked into plugin for redmine              #
+########################################################################
+
+class TaskImport
+  @tasks      = []
+  @project_id = nil
+  @new_categories = []
+  @milestones = []
+  
+  attr_accessor( :tasks, :project_id, :new_categories , :milestones)
+end
